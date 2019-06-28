4ab5d2f902ba7da5eef455c535bb4f9ea2115ee8
Shamim Dewan <49916445+hacksaw390@users.noreply.github.com>
2019-06-28 12:10:00 +0600

    extra

--- index.php
+++ index.php
@@ -1,146 +1,265 @@
 <?php 
 
-$arrayName = array(10,2,5,52,5,5,);
-
-foreach($arrayName as $shamim){
-	echo $shamim."<br>";
-}
-
-
-for ($i=0; $i <=50 ; $i++) { 
-	// if (($i%2)==0) {
-	if (($i%2)==1) {
+// first ami korci
+// ...........................................
+// for ($i=1; $i <=100 ; $i++) { 
+// 	if (($i%2)!=0 && ($i%3)!=0 && ($i%5)!=0 && ($i%7)!=0) {
 		
-	echo $i."<br>";
-	}
-
-
-}
-
-
-$letter='Gf';
-
-	if ($letter=='a'||
-		$letter=='A'||
-		$letter=='e'||
-		$letter=='E'||
-		$letter=='i'||
-		$letter=='I'||
-		$letter=='o'||
-		$letter=='O'||
-		$letter=='u'||
-		$letter=='U') {
-		echo 'This is Vowel';
-	}elseif ( $letter=='b'||
-			  $letter=='B'||
-			  $letter=='c'||
-			  $letter=='C'||
-			  $letter=='b'||
-			  $letter=='D'||
-			  $letter=='f'||
-			  $letter=='F'||
-			  $letter=='g'||
-			  $letter=='G'||
-			  $letter=='h'||
-			  $letter=='H'||
-			  $letter=='j'||
-			  $letter=='J'||
-			  $letter=='k'||
-			  $letter=='K'||
-			  $letter=='l'||
-			  $letter=='L'||
-			  $letter=='m'||
-			  $letter=='M'||
-			  $letter=='n'||
-			  $letter=='N'||
-			  $letter=='p'||
-			  $letter=='P'||
-			  $letter=='q'||
-			  $letter=='Q'||
-			  $letter=='r'||
-			  $letter=='R'||
-			  $letter=='s'||
-			  $letter=='S'||
-			  $letter=='t'||
-			  $letter=='T'||
-			  $letter=='v'||
-			  $letter=='V'||
-			  $letter=='w'||
-			  $letter=='W'||
-			  $letter=='x'||
-			  $letter=='X'||
-			  $letter=='y'||
-			  $letter=='Y'||
-			  $letter=='z'||
-			  $letter=='Z'
-						) {
-		echo 'This is consonant';
-	}
-	else{
-		echo 'You can check only one character';
-	}
-
-
-
-
-$i=1;
-$j=1;
-
-for ($i=1; $i<=10 ; $i++) { 
-
-	echo $i."- multiplication table"."<br>";
-
-	for ($j=1; $j <=10 ; $j++) { 
-	$k=$j*$i;
-
-		echo $i.'x'.$j.' = '.$k."<br>";
-	}
+// 	echo $i."<br>";
+// 	}
 	
-}
+// }
+
+
+// ager kota chilo
+// ..............................................
+
+// for($i=1; $i>=2; $i++)
+// 		{
+// 			if($i==2 || $i==3 || $i==5 || $i==7)
+// 				{
+// 					echo " $i";
+// 				}
+// 			else if(($i%2)!= 0 && ($i%3)!=0 && ($i%5)!=0 && ($i%7)!=0)
+// 				{
+// 					echo " $i";
+// 				}
+// 			else
+// 			continue;
+// 		}
+
+
+// net theke namano
+// ...........................................
+
+// $count = 0;  
+// $num = 2;  
+
+// 	while ($count < 25 ) {  
+// 	$div_count=0;  
+// 		for ( $i=1; $i<=$num; $i++)  {  
+// 			if (($num%$i)==0) { 
+
+// 				$div_count++;  
+// 			}  
+// 		}  
+// 		if ($div_count<3){   
+
+// 			echo $num." , ";  
+// 			$count=$count+1;  
+// 		}
+		  
+// 	$num=$num+1;  
+// 	}  
+
+// .......................................
+// noyon
+// .......................................
+// function prime($n){
+//   if($n == 1)
+//     return 0;
+
+// for($i=2 ; $i <= $n/2 ; $i++){
+
+//     if($n % $i == 0 )
+//       return 0;
+//   }
+//   return 1;
+// }
+// $n = 5;
+
+// $flag = prime($n);
+
+//   if($flag == 1){
+//   echo "Prime";
+//   }else{
+//     echo 'Not Prime';
+//   }
 
 
 
-$i=0;
-$n=50;
 
-while ($i<10) {
-	$n--;
-	$i++;
-}
-echo $n."<br>";
-..............................
-$i=0;
-do {
-	echo $i;
-	$i++;
-} while ($i <= 20);
-.................................
-$i=0;
-while($i <=20){
-	echo $i;
-	$i++;
-}
-.......................?
-$i=20;
-while($i >0){
-	echo $i;
-	$i=$i-2;
-}
+// ..................................................
+// All prime number
+// ..................................................
+// function prime($n){
+//   if($n == 1)
+//     return 0;
 
-.......................................
-for ($i='A'; $i <'Z' ; $i++) { 
-	echo $i."<br>";
-}
-.........................
-for($i=0; $i<=20; $i++){
-	echo $i;
-}
-..........................
-for($i=20; $i>=0; $i=$i-3){
-	echo $i."<br>";
-}
-....................
-for($i=0; $i<=20; $i=$i+2){
-	echo $i."<br>";
-}
+// for($i=2 ; $i <= $n/2 ; $i++){
+
+//     if($n % $i == 0 )
+//       return 0;
+//   }
+//   return 1;
+// }
+
+// for ($n=2; $n <=1000 ; $n++) { 	
+// 	$allprime = prime($n);
+// 	if($allprime == 1)
+// 		echo $n."<br>";
+// }
+// .............................................
+// palindrome
+// .............................................
+
+// $a = 'noyon';
+// $a = 121;
+// $b = strrev($a);
+
+// if ($a == $b) {
+// 	echo "This is palindrome";
+// }else{
+// 	echo 'This is not palindrome';
+// }
+
+
+
+// 100-1000 palindrome number
+
+// for ($i=100; $i <1000 ; $i++) { 
+	
+// 	if($i == strrev($i))
+// 		echo $i.'<br>';
+	
+// }
+
+
+
+
+// ......................................
+// lady
+// ....................................///
+// for($i=1;$i<=100;$i++){
+//   $f=0;
+//   for($j=2;$j<$i;$j++)
+//   {
+//     if(($i%$j)==0){
+//       $f=1;
+//       break;
+// }
+// }
+// if($f==0){
+//   echo $i.'<br>';
+// }
+
+// }
+
+
+
+
+// $n=97;
+// for($i=2;$i<$n;$i++)
+// {
+// $f=0;
+//   if(($n%$i)==0){
+//     $f=1;
+//     break;
+//   }
+// }
+// if($f==0){
+//   echo 'prime number';
+// }
+// else {
+//   echo 'not prime';
+// }
+
+
+
+
+
+
+
+// $a= 6;
+// $b= 1;
+// $c= -1;
+// 	for ($i=$a; $i >=1 ; $i--) {
+
+// 		$b = $b+$i;
+
+// 	}
+// 		echo $b+$c.'<br>';
+
+
+
+
+
+// for ($i='A'; $i <'Z'; $i++) { 
+// 	echo $i.'<br>';
+// }
+
+
+
+
+// $azRange = range('A', 'Z');
+// foreach ($azRange as $letter)
+// {
+//   print("$letter\n");
+// }
+
+
+
+// foreach(range('a','z') as $v){
+//     echo "$v \n";
+// }
+
+// assignment operator
+
+// $x = 10;
+// $y = 20;
+// $z = 4;
+
+// echo $x+=$y;
+// echo '<br>';
+
+// echo $x-=$z;
+// echo '<br>';
+
+// echo $x*=$z;
+// echo '<br>';
+
+// echo $x/=$z;
+// echo '<br>';
+
+// echo $x%=$z;
+
+
+// ocnditional operator
+
+// $a = 10;
+// $b = 20;
+// echo $a==$b;
+
+// $a = 10;
+// $b = 20;
+// echo $a!=$b;
+
+// $a = 10;
+// $b = 10;
+// echo $a==$b;
+
+// $a = 20;
+// $b = '20';
+// echo $a==$b;
+
+// $a = 20;
+// $b = 20;
+// echo $a===$b;
+
+// $a = 20;
+// $b = '20';
+// echo $a===$b;
+
+// $a = 10;
+// $b = '20';
+// echo $a!==$b;
+
+// $a = 20;
+// $b = 20;
+// echo $a!==$b;
+
+
+
+
+
  ?>
